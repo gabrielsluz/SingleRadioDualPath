e2b277ea8ace058155ff467be2b6788b7f9b0cfa
gabrielsluz <36861086+gabrielsluz@users.noreply.github.com>
2019-06-29 15:50:02 -0300

    SImulation finished

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -3,6 +3,8 @@ import sys
 
 class Network:
     _nodes = []
+    _nodes_queue1 = []
+     _nodes_queue2 = []
     _num_nodes = 0
     _source = 0
     _sink = 0
@@ -15,6 +17,8 @@ class Network:
     def __init__(self, num_nodes, source, sink):
         _num_nodes = num_nodes
         for i in range(num_nodes):
+            self._nodes_queue1.append(0)
+            self._nodes_queue2.append(0)
             edges = []
             self._nodes.append(edges[:])
         _source = source
@@ -31,7 +35,8 @@ class Network:
 
     def pick_path(self):
         #If the paths do not have same parity, delete the largest one
-        #If there is a one hop from source to sink and the cost of the one hop is smaller than the multi hop path -> delete the multi hop and add another single hop
+        #If there is a one hop from source to sink and the cost of the one hop is smaller than
+        #the multi hop path -> delete the multi hop and add another single hop
         first_path = self.get_path_length_and_etx(0)
         second_path = self.get_path_length_and_etx(1)
 
@@ -87,24 +92,112 @@ class Network:
             node = edge[0]
 
     def get_new_transmissions(self, transmission_list):
+        new_transmissions = []
+        for i in range(len(transmission_list)):
+            if transmission_list[i][2] <= 0:
+                new_tail = transmission_list[i][1]
+                new_head = self._nodes[new_tail][0][0]
+                new_etx = self._nodes[new_tail][0][1]
+                transmission = (new_tail, new_head, new_etx)
+                new_transmissions.append(transmission)
+        return new_transmissions
+
+    def make_transmissions(self, transmission_list):
+        received_at_sink = 0
+        for i in range(len(transmission_list)):
+            transmission_list[i][2] -= 1
+            if transmission_list[i][2] <= 0:
+                if transmission_list[i][1] == self._sink:
+                    received_at_sink += 1
+                del transmission_list[i]
+
+        return received_at_sink
+
+    def is_transmission_happening(self,transmission, transmission_list):
+        for i in range(len(transmission_list)):
+            if transmission_list[i][0] == transmission[0] and transmission_list[i][1] == transmission[1]:
+                return True
+        return False
+
         
+    def add_new_transmissions1(self, new_transmissions, transmission_list):
+        for i in range(len(new_transmissions)):
+            if self.is_transmission_happening(new_transmissions[i], transmission_list):
+                self._nodes_queue1[transmission[1]] += 1
+            else:
+                transmission_list.append(new_transmissions[i])
+
+    def add_new_transmissions2(self, new_transmissions, transmission_list):
+        for i in range(len(new_transmissions)):
+            if self.is_transmission_happening(new_transmissions[i], transmission_list):
+                self._nodes_queue2[transmission[1]] += 1
+            else:
+                transmission_list.append(new_transmissions[i])
+    
+    def send_from_queues1(self, transmission_list):
+        for i in range(1, _num_nodes):
+            if self._nodes_queue1[i] > 0:
+                tail = i
+                head = self._nodes[tail][0][0]
+                etx = self._nodes[tail][0][1]
+                transmission = (tail, head, etx)
+                if self.is_transmission_happening(transmission, transmission_list):
+                    transmission_list.append(transmission)
+                    self._nodes_queue1[i] -= 1
+    
+    def send_from_queues2(self, transmission_list):
+        for i in range(1, _num_nodes):
+            if self._nodes_queue2[i] > 0:
+                tail = i
+                head = self._nodes[tail][0][0]
+                etx = self._nodes[tail][0][1]
+                transmission = (tail, head, etx)
+                if self.is_transmission_happening(transmission, transmission_list):
+                    transmission_list.append(transmission)
+                    self._nodes_queue2[i] -= 1
+
+            
 
 
 
 
-    def simulate(self):
+
+
+    def simulate(self, max_time):
         transmission_list0 = []
         transmission_list1 = []
         time = 0
         msgs_received = 0
 
-        while True:
+        while time < max_time:
             if time % 2 == 0:
                 #Check which transmissions ended in list 1, and make transmissions (decrement counter) in list 0
                 #Send from source to first path
+                #Send from the nodes that can send now (have a packet in queue and are from this interval)
                 new_transmissions = []
                 trans_source = (_source, self._nodes[self._source][0][0], self._nodes[self._source][0][1])
                 new_transmissions.append(trans_source)
+                new_transmissions.extend(self.get_new_transmissions(transmission_list1))
+
+                self.add_new_transmissions1(new_transmissions, transmission_list0)
+                self.send_from_queues1(transmission_list0)
+                msgs_received += self.make_transmissions(transmission_list0)
+            else:
+                #Check which transmissions ended in list 0, and make transmissions (decrement counter) in list 1
+                #Send from source to second path
+                #Send from the nodes that can send now (have a packet in queue and are from this interval)
+                new_transmissions = []
+                trans_source = (_source, self._nodes[self._source][1][0], self._nodes[self._source][1][1])
+                new_transmissions.append(trans_source)
+                new_transmissions.extend(self.get_new_transmissions(transmission_list0))
+
+                self.add_new_transmissions2(new_transmissions, transmission_list1)
+                self.send_from_queues2(transmission_list1)
+                msgs_received += self.make_transmissions(transmission_list1)
+
+            time += 1
+        self._throughput = float(msgs_received )/ float(max_time)
+
 
 
 
